ac910d69ac497e155e438d70d6579a93b8221bcc
Prashanth Reddy <39459337+prashanthr11@users.noreply.github.com>
2020-07-01 22:48:55 +0530

    Update ATTND.py

--- Practice/ATTND.py
+++ Practice/ATTND.py
@@ -1,10 +1,13 @@
 from collections import defaultdict
+
 for i in range(int(input())):
     l = list()
     for j in range(int(input())):
         l.append(list(map(str, input().split())))
+        
     d = defaultdict(int)
     l2 = list()
+    
     for i in range(len(l)):
         if d[l[i][0]]:
             l2.append(l[i][0])
@@ -17,4 +20,3 @@ for i in range(int(input())):
             print(temp)
         else:
             print(l[i][0])
-        
