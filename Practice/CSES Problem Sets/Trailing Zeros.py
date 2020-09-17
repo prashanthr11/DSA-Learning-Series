b5cd168554b09daa02bc9c7b043b442ce29cb530
prashanth <prashanthr6789@gmail.com>
2020-09-17 10:51:03 +0000

    files moved

--- Practice/CSES Problem Sets/Trailing Zeros.py
+++
@@ -1,7 +0,0 @@
-n = int(input())
-ans = 0
-i = 5
-while i <= n:
-    ans += int(n / i)
-    i *= 5
-print(ans)
--- Practice/Sort Names.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-
-l = list()
-for i in range(n):
-    s = input()
-    l.append(s)
-    
-for i in sorted(l):
-    print(i, end=' ')
